84b8467509b31c0255d6adf7c309ec4a3d507060
JoonikChoi <omybell201@gmail.com>
2022-10-26 18:39:04 +0900

    Mediasoup Test Try 1

--- toServer/API-AccessPoint.js
+++ toServer/API-AccessPoint.js
@@ -1,10 +1,13 @@
 
 // S3 Push
-export const socketPoint = `https://api.portal301.com`;
-export const loginPoint = `https://api.portal301.com/login/session_id`;
-export const logoutPoint = `https://api.portal301.com/login/session`;
+export const socketPoint = `https://43.201.110.62:3333`;
+// export const loginPoint = `https://api.portal301.com/login/session_id`;
+// export const logoutPoint = `https://api.portal301.com/login/session`;
 
 // Development and testing
-// export const socketPoint = `https://192.168.0.12:3333`;
-// export const loginPoint = `https://192.168.0.12:3333/login/session_id`;
-// export const logoutPoint = `https://192.168.0.12:3333/login/session`;
+//export const socketPoint = `https://192.168.56.1:3333`;
+export const loginPoint = `https://192.168.0.12:3333/login/session_id`;
+export const logoutPoint = `https://192.168.0.12:3333/login/session`;
+
+//mediasoup server aws
+//https://43.201.110.62:3333/publish.html
