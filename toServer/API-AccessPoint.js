fa201301c94351bcb388220ee0eafa10eb82f2ed
JoonikChoi <omybell201@gmail.com>
2022-10-28 21:14:55 +0900

    try 66

--- toServer/API-AccessPoint.js
+++ toServer/API-AccessPoint.js
@@ -3,7 +3,7 @@
 export const socketPoint = `https://api.portal301.com/`;
 export const loginPoint = `https://api.portal301.com/login/session_id`;
 export const logoutPoint = `https://api.portal301.com/login/session`;
-export const MediaSoupSocketPoint = `https://13.124.32.140:3333`;
+export const MediaSoupSocketPoint = `https://3.34.139.14:3333`;
 
 // Development and testing
 //export const socketPoint = `https://192.168.56.1:3333`;
@@ -11,5 +11,5 @@ export const MediaSoupSocketPoint = `https://13.124.32.140:3333`;
 //export const logoutPoint = `https://192.168.0.12:3333/login/session`;
 //export const MediaSoupSocketPoint = `https://localhost:3333`;
 
-//mediasoup server aws
-//https://43.201.110.62:3333/publish.html
+//mediasoup server aws ec2
+//https://13.124.32.140:3333/publish.html
