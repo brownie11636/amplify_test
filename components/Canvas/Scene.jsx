f52c83ed5dbf3ada72879ffc8bc19300d317a604
yeosangyoon <brownie11636@gmail.com>
2022-11-16 20:42:52 +0900

    fix height of banner and three canvas

--- components/Canvas/Scene.jsx
+++ components/Canvas/Scene.jsx
@@ -1,21 +1,23 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload } from '@react-three/drei'
 import dynamic from 'next/dynamic'
-
+import styles from "./Scene.module.css"
 
 const Blob = dynamic(() => import('./Blob'), { ssr: false })
 
 export default function Scene() {
   // Everything defined in here will persist between route changes, only children are swapped
   return (
-    <Canvas>
-    <directionalLight intensity={0.75} />
-    <ambientLight intensity={0.75} />
+    <div className={styles.canvasContainer}>
+      <Canvas>
+        <directionalLight intensity={0.75} />
+        <ambientLight intensity={0.75} />
 
-    <Blob route='/' position-y={-0.75} />
+        <Blob route='/' position-y={-0.75} />
 
-    <Preload all />
-    <OrbitControls />
-  </Canvas>
+        <Preload all />
+        <OrbitControls />
+      </Canvas>
+    </div>
   )
 }
