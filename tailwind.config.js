bd33c217869e3bb023cb1118c598dbb747c25a13
HS-Jeong88 <wjdgksto88@gmail.com>
2023-05-24 10:26:54 +0900

    1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
-const tailwindcss = require("tailwindcss");
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  darkMode: "class", // default 'media', if change 'class', add className 'dark' to parent element
   plugins: [],
 };
