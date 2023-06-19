11417851ad155c58be76168ae0154e1dcbdc8649
unknown <dohyung1214@gmail.com>
2023-06-19 23:52:44 +0900

    4 byte to decimal trying

--- components/Test/Sensor/seriesParsers.js
+++ components/Test/Sensor/seriesParsers.js
@@ -6,35 +6,67 @@ export default function parser(packet, parser_id, targetSensor){
     let array1=[];
     let array2=[];
     let hexArray;
+    let timeBinary = "";
+    let dtBinary = "";
+    let xBinary = "";
+    let yBinary = "";
+    let zBinary = "";
 
     packet.forEach((e,i)=>{
-        // console.log(e.payload); 
-        hexArray = e.payload.split("").map(char => parseInt(char.charCodeAt(0),10));
-        // console.log("hexArray:"+hexArray);
-        console.log("i:"+i);
-        hexArray.forEach((datum,idx)=>{
-          console.log(idx);
-          if(idx%3 == 0){
-            array0.push([i*192+idx,datum])
-          }else if(idx%3 == 1){
-            array1.push([i*192+idx,datum])
-          }else if(idx%3 == 2){
-            array2.push([i*192+idx,datum])
+      console.log(e.receivedData);
+      hexArray = e.receivedData.split("").map(char => parseInt(char.charCodeAt(0),10));
+      console.log(hexArray);
+      hexArray.forEach((datum,idx)=>{
+        if (idx<4) {
+          console.log(datum.toString(2));
+          timeBinary += datum.toString(2);
+        }
+        if (idx>3 && idx<8) {
+          dtBinary += datum.toString(2);
+        }
+        if (idx>8) {
+          if ((idx-4)%12<4) {
+            xBinary += datum.toString(2);
+            if ((idx-4)%12 == 3) {
+              array0.push(parseInt(xBinary,2));
+              xBinary = "";
+            }
           }
-        })
-        console.log(array0);
-        console.log([{
-            name: targetSensor+".x",
-            data:array0
-          },
-          {
-            name:targetSensor+".y",
-            data:array1
-          },
-          {
-            name:targetSensor+".z",
-            data:array2
-          }]);
+          if ((idx-4)%12>3 && (idx-4)%12<8) {
+            yBinary += datum.toString(2);
+            if ((idx-4)%12 == 7) {
+              array1.push(parseInt(yBinary,2));
+              yBinary = "";
+            }
+          }
+          if ((idx-4)%12 >7) {
+            zBinary += datum.toString(2);
+            if ((idx-4)%12 == 11) {
+              array2.push(parseInt(zBinary,2));
+              zBinary = "";
+            }
+          }
+        }
+      })
+
+      console.log("t: "+parseInt(timeBinary,2));
+      console.log("dt: "+parseInt(dtBinary,2));
+      console.log("x: "+array0);
+      console.log("y: "+array1);
+      console.log("z: "+array2);
+      
+      // console.log([{
+      //   name: targetSensor+".x",
+      //   data:array0
+      // },
+      // {
+      //   name:targetSensor+".y",
+      //   data:array1
+      // },
+      // {
+      //   name:targetSensor+".z",
+      //   data:array2
+      // }]);
     })
     return([{
       name: targetSensor+".x",
--- pages/test/sensor/index.jsx
+++ pages/test/sensor/index.jsx
@@ -72,65 +72,6 @@ const Test = ()=> {
       }
     ]
   }
-
-  // const parserId = "1q2w3e4r";
-
-  // const parser = (packet, parser_id)=>{
-  //   if(parser_id=="1q2w3e4r"){
-  //     parser_1q2w3e4r(packet);      
-  //   }
-  // }
-
-  // const parser_1q2w3e4r = (packet)=>{
-  //   let array0=[];
-  //   let array1=[];
-  //   let array2=[];
-  //   let hexArray;
-
-  //   packet.forEach((e,i)=>{
-  //       console.log(e.payload); 
-  //       hexArray = e.payload.split("").map(char => parseInt(char.charCodeAt(0),10));
-  //       console.log(hexArray);
-  //       hexArray.forEach((datum,idx)=>{
-  //         if(idx%3 == 0){
-  //           array0.push([i*20+idx,datum])
-  //         }else if(idx%3 == 1){
-  //           array1.push([i*20+idx,datum])
-  //         }else if(idx%3 == 2){
-  //           array2.push([i*20+idx,datum])
-  //         }
-  //       })
-  //       setChartSeries([{
-  //         name: targetSensor+".x",
-  //         data:array0
-  //       },
-  //       {
-  //         name:targetSensor+".y",
-  //         data:array1
-  //       },
-  //       {
-  //         name:targetSensor+".z",
-  //         data:array2
-  //       }
-  //     ]);
-  //     console.log([{
-  //       name: targetSensor+".x",
-  //       data:array0
-  //     },
-  //     {
-  //       name:targetSensor+".y",
-  //       data:array1
-  //     },
-  //     {
-  //       name:targetSensor+".z",
-  //       data:array2
-  //     }
-  //   ])
-  //       // console.log("\n"); 
-  //       // console.log(hexArray.map(dec => dec.toString(16)));
-  //       // console.log("\n"); 
-  //   })
-  // };
   
   const [targetDevice, setTargetDevice] = useState("perpet/SerialNumber/")
   const [targetSensor, setTargetSensor] = useState("acc")
