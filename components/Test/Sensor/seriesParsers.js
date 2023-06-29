3ac92a882276b12d9b2980dd3e453f1569afe45a
DohyungKim <dohyung1214@gmail.com>
2023-06-29 16:49:28 +0900

    pressure, need to change int to float

--- components/Test/Sensor/seriesParsers.js
+++ components/Test/Sensor/seriesParsers.js
@@ -1,6 +1,6 @@
 
 export default function parser(packet, parser_id){
-
+ 
   if(parser_id=="1q2w3e4r") {
     let hexArray = [];
     let timeHex = "";
@@ -61,17 +61,69 @@ export default function parser(packet, parser_id){
     const dataArray2 = timeArray.map((time, index) => {
       return ([time, array2[index]]);
     });
-
-    return ({x:[
+    return ([
       {
         "name": "x",
         "data": dataArray0
-      }], y:[{
+      },{
       "name": "y",
       "data": dataArray1
-    }], z:[{
+    },{
       "name": "z",
       "data": dataArray2
-    }]});
+    }]);
+  }
+
+  if(parser_id=="pressure") {
+    let hexArray = [];
+    let timeHex = "";
+    let lengthHex = "";
+    let timeArray = [];
+    let pressureHex = "";
+    let pressureArray = [];
+    let dt = 200;
+
+    packet.reverse().forEach((e)=>{
+      for (let i = 0; i < e.payload.length; i += 2) {
+        hexArray.push(e.payload[i] + e.payload[i + 1]);
+      }
+      hexArray.forEach((datum,idx)=>{
+        if (idx<4) {
+          timeHex += datum;
+          if (idx==3) {
+            timeArray.push(parseInt(timeHex,16));
+          }
+        }
+        if (idx>3 && idx<6) {
+          lengthHex += datum;
+          if (idx==5) {
+            console.log(parseInt(lengthHex,16));
+            for (let j=0;j<parseInt(lengthHex,16)-1;j++) {
+              timeArray.push(timeArray.at(-1)+dt);
+            }
+          }
+        }
+        if (idx>5) {
+          pressureHex += datum;
+          if ((idx-6)%4 == 3) {
+            pressureArray.push(parseInt(pressureHex,16));
+            pressureHex = "";
+          }          
+        }
+      })
+      timeHex = "";
+      lengthHex = "";
+      hexArray = [];
+    })
+
+    const dataArray = timeArray.map((time, index) => {
+      return ([time, pressureArray[index]]);
+    });
+    console.log
+    return ([
+      {
+        "name": "pressure",
+        "data": dataArray
+      }]);
   }
 }
