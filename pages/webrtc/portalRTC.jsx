3f091a7d6e4e542ec93fd41834e4666f6c6f61f1
JoonhwaChoi <pass2774@gmail.com>
2023-08-09 13:36:45 +0900

    minor

--- pages/webrtc/portalRTC.jsx
+++ pages/webrtc/portalRTC.jsx
@@ -24,8 +24,6 @@ export default function portalRTC(){
                     </WebrtcContainer>
                 </MainLayout>            
             </PortalCommContext.Provider>
-
-   
     );
 }
 
