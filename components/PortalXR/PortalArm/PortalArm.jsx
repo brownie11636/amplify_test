978a6bfd4c3cc26f5f64c9591f678ade38a908a2
brownie11636 <brownie11636@gmail.com>
2023-08-14 17:10:30 +0900

    add gripping control

--- components/PortalXR/PortalArm/PortalArm.jsx
+++ components/PortalXR/PortalArm/PortalArm.jsx
@@ -57,6 +57,8 @@ export default function PortalArm(type, path, ...props) {
   const controllerMode = useRef(useModeStore.getState().controllerMode);
   const stickUp_R = useRef(useXRGamepadStore.getState().stickUp_R)
   const stickDown_R = useRef(useXRGamepadStore.getState().stickDown_R)
+  const stickLeft_R = useRef(useXRGamepadStore.getState().stickLeft_R)
+  const stickRight_R = useRef(useXRGamepadStore.getState().stickRight_R)
 
   const gripDistanceRef = useRef(useControlStore.getState().gripDistance)
   const increaseGripDistance = useControlStore((state)=>state.increaseGripDistance);
@@ -83,6 +85,9 @@ export default function PortalArm(type, path, ...props) {
     
     console.log("socketid in PortalArm.jsx")
     console.log(commClientV01.socket.id)
+
+    // useControlStore.setState({actualAngles_q:[60*DEG2RAD,-90*DEG2RAD,90*DEG2RAD,0,30*DEG2RAD,0]})
+    // useControlStore.setState({actualAngles_q:[77.1*DEG2RAD,-106.47*DEG2RAD,128.79*DEG2RAD,-22.15*DEG2RAD,77*DEG2RAD,30.2*DEG2RAD]})
 
     commClientV01.socket.on("robot",(type, packet)=>{
       console.log(type)
@@ -126,13 +131,14 @@ export default function PortalArm(type, path, ...props) {
       squeezePressed_R.current.now = state.squeezePressed_R
       stickUp_R.current = state.stickUp_R;
       stickDown_R.current = state.stickDown_R;
+      stickLeft_R.current = state.stickLeft_R;
+      stickRight_R.current = state.stickRight_R;
     })
     const unsubModeStore = useModeStore.subscribe((state) => {
       controllerMode.current = state.controllerMode
     })
     
-    useControlStore.setState({actualAngles_q:[60*DEG2RAD,-90*DEG2RAD,90*DEG2RAD,0,30*DEG2RAD,0]})
-
+    
     return () => {
       commClientV01.socket.off("robot")
       console.log("robot comm off")
@@ -152,6 +158,7 @@ export default function PortalArm(type, path, ...props) {
   let storeDistance
   let dir = 1
   let i = 0;
+  let iXR = 0;
 
   useFrame((state, delta, XRFrame)=> { 
 
@@ -170,14 +177,28 @@ export default function PortalArm(type, path, ...props) {
         console.log(controlGuideRef.current.rotation)
     }
 
+    if(i<3 && XRFrame){
+      if(commClientV01.connectedModules){
+        //request _q msg (robot joint angles) for initial angle
+        packet = { 
+          from: commClientV01.socket.id, 
+          to:commClientV01.connectedModules[0],
+          msg: {
+            type:"get_pos"
+          } 
+        }
+        commClientV01.socket.emit("robot","C2C",packet)
+      }
+    }
+
     // ang += delta;
     // useControlStore.setState({actualAngles_q:[ang*DEG2RAD,-90*DEG2RAD,90*DEG2RAD,0,30*DEG2RAD,0]})
     // useControlStore.setState({actualAngles_q:[60*DEG2RAD,-90*DEG2RAD,90*DEG2RAD,0,30*DEG2RAD,0]})
     // updateActualAngles_q([ang,-90,90,0,30,0])
     // console.log(storeDistance)
-    if (gripDistanceRef.current >= 100) dir = -1
-    if (gripDistanceRef.current <= 5) dir = 1 
-    increaseGripDistance(dir*delta*10)
+    // if (gripDistanceRef.current >= 100) dir = -1
+    // if (gripDistanceRef.current <= 5) dir = 1 
+    // increaseGripDistance(dir*delta*10)
 
     // controlGuideRef.current.position.copy(posTest)
     if(XRFrame){
@@ -188,67 +209,76 @@ export default function PortalArm(type, path, ...props) {
         // console.log(acculTime)
         acculTime = 0
         if (controllerMode.current === "operating" && rightController){
-            // controlGuideRef.current.position.copy(
-            //   armRef.current[0].worldToLocal(           //convert to local  
-            //     rightController.controller.position));  //world position
-            // controlGuideRef.current.rotation.copy(rightController.controller.rotation);
+          // controlGuideRef.current.position.copy(
+          //   armRef.current[0].worldToLocal(           //convert to local  
+          //     rightController.controller.position));  //world position
+          // controlGuideRef.current.rotation.copy(rightController.controller.rotation);
 
-            if (squeezePressed_R.current.now === true){  //squeeze
-              
-              //controller 6DOF
-              
-              // 컨트롤러 위치를 가져옴
-              // pos = armRef.current.children[0].worldToLocal(
-              //   rightController.controller.position).toArray();
-              
-              // controlGuideRef 포지션을 가져옴
-              pos = armRef.current[1].worldToLocal(     // base model coordinate
-                controlGuideRef.current.getWorldPosition(vec3)).toArray();
+          if (squeezePressed_R.current.now === true){  //squeeze
+            
+            //controller 6DOF
+            
+            // 컨트롤러 위치를 가져옴
+            // pos = armRef.current.children[0].worldToLocal(
+            //   rightController.controller.position).toArray();
+            
+            // controlGuideRef 포지션을 가져옴
+            pos = armRef.current[1].worldToLocal(     // base model coordinate
+              controlGuideRef.current.getWorldPosition(vec3)).toArray();
 
-              pos = [-pos[0], pos[2], pos[1]];
-              pos = pos.map((val) => Math.round(val*10000)/10000)
+            pos = [-pos[0], pos[2], pos[1]];
+            pos = pos.map((val) => Math.round(val*10000)/10000)
+            
+            // vrLog("x: "+pos[0]+", y: "+pos[1]+",z: "+pos[2]);
               
-              // vrLog("x: "+pos[0]+", y: "+pos[1]+",z: "+pos[2]);
-                
-              euler.copy(controlGuideRef.current.rotation.clone());
-              // euler.copy(controlGuideRef.current.rotation).reorder("ZYX");
-              // euler.x -= 0.5 * Math.PI;
-              // euler.reorder("XZY");
-              // rot = euler.toArray()
-              rot = euler.reorder("XZY").toArray()
-              vrLog(rot[3]);
-              rot = rot.map((val)=>Math.round(val*10000)/10000)
-              // rot = euler.toArray().map((val)=>Math.round(val*10000)/10000)
-              rot = [-rot[0], rot[2], rot[1]]
-              // rot = [-rot[0], -rot[1], rot[2]]
-              // console.log(typeof rot[0], typeof rot[1], typeof rot[2])
-              vrLog("rot x: "+rot[0]+", y: "+rot[1]+",z: "+rot[2]);
+            euler.copy(controlGuideRef.current.rotation.clone());
+            // euler.copy(controlGuideRef.current.rotation).reorder("ZYX");
+            // euler.x -= 0.5 * Math.PI;
+            // euler.reorder("XZY");
+            // rot = euler.toArray()
+            rot = euler.reorder("XZY").toArray()
+            vrLog(rot[3]);
+            rot = rot.map((val)=>Math.round(val*10000)/10000)
+            // rot = euler.toArray().map((val)=>Math.round(val*10000)/10000)
+            rot = [-rot[0], rot[2], rot[1]]
+            // rot = [-rot[0], -rot[1], rot[2]]
+            // console.log(typeof rot[0], typeof rot[1], typeof rot[2])
+            vrLog("rot x: "+rot[0]+", y: "+rot[1]+",z: "+rot[2]);
 
-              packet = { 
-                from: commClientV01.socket.id, 
-                to:commClientV01.connectedModules[0],
-                msg:{
-                  type:"set_pos", 
-                  data:{
-                    arm:[...pos,...rot],
-                    grip: Math.floor(gripDistanceRef.current * 10) 
-                  }
-                }
-              } 
-              console.log("send C2C packet")
-              // console.log(packet.msg.data.arm);
-              commClientV01.socket.emit("robot", "C2C", packet, (res) => {
-              // console.log("msg-v0 response:",res)
-              })
+            if (stickRight_R.current) {
+            // if (stickUp_R.current) {
+              increaseGripDistance(-30 * delta)
+            } else if (stickLeft_R.current) {
+            // } else if (stickDown_R.current) {
+              vrLog("close")
+              increaseGripDistance(30 * delta)
             }
+
+            packet = { 
+              from: commClientV01.socket.id, 
+              to:commClientV01.connectedModules[0],
+              msg:{
+                type:"set_pos", 
+                data:{
+                  arm:[...pos,...rot],
+                  grip: Math.floor(gripDistanceRef.current * 10) 
+                }
+              }
+            } 
+            console.log("send C2C packet")
+            // console.log(packet.msg.data.arm);
+            commClientV01.socket.emit("robot", "C2C", packet, (res) => {
+            // console.log("msg-v0 response:",res)
+            })
           }
+        }
 
-        if (stickUp_R === true || stickDown_R === true ) {
-          if(stickUp_R === true) dir = 1;
-          else if (stickDown_R) dir = -1;
+        // if (stickUp_R === true || stickDown_R === true ) {
+        //   if(stickUp_R === true) dir = 1;
+        //   else if (stickDown_R) dir = -1;
 
-          increaseGripDistance(dir * 30 * delta)
-        }     
+        //   increaseGripDistance(dir * 30 * delta)
+        // }     
       }
     }
 
