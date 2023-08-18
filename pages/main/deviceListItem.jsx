c464d6844eb9a97390f6ab4f5312c996a435b4c0
HS-Jeong88 <wjdgksto88@gmail.com>
2023-08-18 17:11:48 +0900

    1

--- pages/main/deviceListItem.jsx
+++ pages/main/deviceListItem.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import styles from './main.module.css';
+import React from "react";
+import styles from "./main.module.css";
 
 const ListItem = ({ device }) => {
   return (
-    <div className={styles.item}>
-      <div className={styles.deviceName}>{device.name}</div>
-      <div className={styles.serialNumber}>{device.serialNumber}</div>
-      {/* <button className={styles.addButton}>+</button> */}
+    <div className={styles?.item}>
+      <div className={styles?.deviceName}>{device?.name}</div>
+      <div className={styles?.serialNumber}>{device?.serialNumber}</div>
+      {/* <button className={styles?.addButton}>+</button> */}
     </div>
   );
 };
