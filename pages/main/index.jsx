caf31678b0eaa69911dc1e52ded9dd1589771883
HS-Jeong88 <wjdgksto88@gmail.com>
2023-08-28 06:47:57 +0900

    1

--- pages/main/index.jsx
+++ pages/main/index.jsx
@@ -11,8 +11,10 @@ import { useSession } from "next-auth/react";
 
 import PltModuleRoster from "./PltModuleRoster";
 import PltTaskRoster from "./PltTaskRoster";
+import { useRouter } from "next/router";
 
 const Main = () => {
+  const router = useRouter();
   // Sample data for devices and tasks (replace with your actual data)
   const [moduleList, setModuleList] = useState([
     // { alias: 'Device 1', serialNumber: '12345' , type: 'Robot', manufacturer: 'Universal Robots'},
@@ -91,44 +93,66 @@ const Main = () => {
   // );
   // const [robotItemList, SetRobotItemList] = useRecoilState(RobotItemListAtom);
 
+  // Simulate fetching data or changing the list dynamically
+  // For example, fetchDevices and fetchTasks could be API calls
+  const fetchDevices = async () => {
+    await axios
+      .post(baseURL + "/api/mongo/robotList", {
+        companyNumber:
+          session?.token?.user?.affiliation === "admin"
+            ? "admin"
+            : session?.token?.user?.affiliation,
+      })
+      .then((res) => {
+        console.log(res?.data?.data);
+        // setModuleList(res?.data?.data?);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert(err?.response?.data?.msg);
+        // return router.push(`/main/login`);
+      });
+  };
+  const fetchModuleList = async () => {
+    // curl -k -X POST -H "Content-Type: application/json" -d '{"filter":{}}' https://localhost:3333/portalfetch/module-list
+    await axios
+      .post(baseURL + "/api/fetch/v0.1/module/module-list", {
+        filter: {},
+      })
+      .then((res) => {
+        console.log(res?.data?.data);
+        setModuleList(res?.data?.data?.reverse());
+      })
+      .catch((err) => {
+        console.log(err);
+        alert(err?.response?.data?.msg);
+        return router.push(`/main/login`);
+      });
+  };
+  const fetchTasks = async () => {
+    // curl -k -X POST -H "Content-Type: application/json" -d '{"filter":{}}' https://localhost:3333/portalfetch/module-list
+    await axios
+      .post(baseURL + "/api/fetch/v0.1/task/list", {
+        filter: {},
+      })
+      .then((res) => {
+        console.log(res?.data?.data);
+        setTasks(res?.data?.data?.reverse());
+      })
+      .catch((err) => {
+        console.log(err);
+        alert(err?.response?.data?.msg);
+        return router.push(`/main/login`);
+      });
+
+    // Fetch tasks from an API and update the tasks state
+    // const fetchedTasks = await fetchTasksFromAPI();
+    // const fetchedTasks = ['Task 1', 'Task 2', 'Task 3'];
+  };
   useEffect(() => {
     if (baseURL) {
-      // Simulate fetching data or changing the list dynamically
-      // For example, fetchDevices and fetchTasks could be API calls
-      const fetchDevices = async () => {
-        const response = await axios.post(baseURL + "/api/mongo/robotList", {
-          companyNumber:
-            session?.token?.user?.affiliation === "admin"
-              ? "admin"
-              : session?.token?.user?.affiliation,
-        });
-        if (response.data?.data) console.log(response.data?.data);
-        // setModuleList(response.data?.data);
-
-        // curl -k -X POST -H "Content-Type: application/json" -d '{"filter":{}}' https://localhost:3333/portalfetch/module-list
-        // const fetchedDevices = await axios.post(baseURL+"/fetch/v0.1/module/module-list", {
-        const fetchedDevices = await axios.post(baseURL + "/api/portalfetch/module/module-list", {
-          filter: {},
-        });
-        if (fetchedDevices?.data) console.log(fetchedDevices?.data?.data);
-        setModuleList(fetchedDevices?.data?.data?.reverse());
-      };
-
-      const fetchTasks = async () => {
-        // curl -k -X POST -H "Content-Type: application/json" -d '{"filter":{}}' https://localhost:3333/portalfetch/module-list
-        // const fetchedTasks = await axios.post(baseURL+"/fetch/v0.1/task/list", {
-        const fetchedTasks = await axios.post(baseURL + "/api/portalfetch/task/list", {
-          filter: {},
-        });
-        if (fetchedTasks?.data) console.log(fetchedTasks?.data?.data);
-
-        // Fetch tasks from an API and update the tasks state
-        // const fetchedTasks = await fetchTasksFromAPI();
-        // const fetchedTasks = ['Task 1', 'Task 2', 'Task 3'];
-        setTasks(fetchedTasks?.data?.data?.reverse());
-      };
-
       fetchDevices();
+      fetchModuleList();
       fetchTasks();
     }
   }, [session, baseURL]); // Empty dependency array to run the effect only once
@@ -136,8 +160,8 @@ const Main = () => {
   return (
     <MainLayout>
       <div className={styles.container}>
-        <PltModuleRoster />
-        <PltTaskRoster />
+        {/* <PltModuleRoster />
+        <PltTaskRoster /> */}
       </div>
     </MainLayout>
   );
